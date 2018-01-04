3fe357520c229f8a65f78b87fdb8e0a6bb23ff54
James Garner <j@jada.io>
2018-01-04 00:41:12 +0000

    fix building as a react dependency, jesus christ

---
+++ dist.js
@@ -0,0 +1,44 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getSiteUrl = getSiteUrl;
+exports.getApiUrl = getApiUrl;
+exports.getEuphoriaUrl = getEuphoriaUrl;
+exports.openEuphoria = openEuphoria;
+exports.openPublisherPlaybook = openPublisherPlaybook;
+exports.openIntegrationGuide = openIntegrationGuide;
+function getSiteUrl() {
+  if (window.location.protocol === 'http:') {
+    return 'http://localhost:4444';
+  } else {
+    return 'https://euphoricadventur.es';
+  }
+}
+
+function getApiUrl() {
+  return window.location.protocol + '//jada.io/euphoria-api';
+}
+
+function getEuphoriaUrl(email) {
+  if (window.location.protocol === 'http:') {
+    return 'http://localhost:8080/#!/inbound/log-in-email/' + email;
+  } else {
+    return 'https://euphoricadventur.es/app/index.html#!/inbound/log-in-email/' + email;
+  }
+}
+
+function openEuphoria(email) {
+  window.open(getEuphoriaUrl(email), '_blank');
+}
+
+function openPublisherPlaybook() {
+  window.open(getSiteUrl() + '/integration-guide', '_blank');
+  // this guide hasn't been written yet
+  // window.open(`${getSiteUrl()}/publisher-playbook`, '_blank')
+}
+
+function openIntegrationGuide() {
+  window.open(getSiteUrl() + '/integration-guide', '_blank');
+}
